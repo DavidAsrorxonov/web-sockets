28bfc781f60b0fab73ba21e34b501ce22028f896
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-02-01 23:05:12 +0900

    chore: switch test app to ESM and add WS/Node types - set "type":"module" for ESM, add @types/node and @types/ws dev deps, add initial WebSocket server scaffold

--- test/server.js
+++ test/server.js
@@ -1,6 +1,18 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 // Connection Event
-wss.on("connection", (socket, request) => {});
+wss.on("connection", (socket, request) => {
+  const ip = request.socket.remoteAddress;
+
+  socket.on("message", (raw) => {
+    const message = raw.toString();
+    console.log(raw);
+
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN)
+        client.send(`Server broadcast: ${message}`);
+    });
+  });
+});
